a09d12e81a236bd20c58af736a3776b075af0df6
salpiglossis <salpiglossis@kazbox.subsub>
2026-02-23 02:29:02 -0500

    Refactor: moved repo root to produktik247

--- app/main.py
+++ app/main.py
@@ -585,10 +585,8 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "simple_bot_aiogram:app",
+        "main:app",
         host="0.0.0.0",
         port=PORT,
-        ssl_keyfile="privkey.pem",
-        ssl_certfile="fullchain.pem",
         log_level="error"
     )
